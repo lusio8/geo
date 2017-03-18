d1bf15d169e1550da80cdf3fea760b2f292ee498
lucio bria <lucio.bria@gmail.com>
2017-03-18 15:21:50 +0100

    first commit

---
+++ Geo/Geo/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace Geo.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Geo/Geo/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using Geo.ViewModels;
+
+using Xamarin.Forms;
+
+namespace Geo.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+		{
+			InitializeComponent();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+	}
+}
